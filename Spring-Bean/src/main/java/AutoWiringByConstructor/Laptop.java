d665272e4436ee90fa174cc6df4870be7e535e57
Rakesh0045 <rakeshparida2801@gmail.com>
2025-07-17 21:18:46 +0530

    Autowiring - by constructor

---
+++ Spring-Bean/src/main/java/AutoWiringByConstructor/Laptop.java
@@ -0,0 +1,14 @@
+package AutoWiringByConstructor;
+
+public class Laptop {
+
+    private Specification specification;
+
+    public Laptop(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void displayInfo() {
+        System.out.println("Laptop details: " + specification.toString());
+    }
+}
