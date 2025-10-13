4878e49f8dd9523a099f72137b3330400888699b
Rakesh0045 <rakeshparida2801@gmail.com>
2025-10-13 08:51:18 +0530

    Initial commit

---
+++ blog-platform/blog-platform-backend/src/test/java/com/blogapp/blog_platform_backend/BlogPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogapp.blog_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
