234de6f1d40e3a8ec40a487cfaf8da5f9ab2bda1
Rakesh0045 <rakeshparida2801@gmail.com>
2025-10-13 09:57:15 +0530

    add repositories

---
+++ blog-platform/blog-platform-backend/src/main/java/com/blogapp/blog_platform_backend/domain/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.blogapp.blog_platform_backend.domain.repositories;
+
+import com.blogapp.blog_platform_backend.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
