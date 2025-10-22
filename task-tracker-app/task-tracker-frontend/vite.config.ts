f99b973289ec5f76581fa6fc6a4db96cb02eb601
Rakesh Kumar Parida <150229028+Rakesh0045@users.noreply.github.com>
2025-10-22 12:18:41 +0530

    Refactor Vite config to load environment variables

--- task-tracker-app/task-tracker-frontend/vite.config.ts
+++ task-tracker-app/task-tracker-frontend/vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://spring-taskapp-backend.onrender.com',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+
+  const DEV_BACKEND = env.VITE_DEV_BACKEND_URL ?? 'http://localhost:2801'
+
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: DEV_BACKEND,
+          changeOrigin: true,
+        }
       }
+    },
+    define: {
+      'process.env': {}
     }
   }
 })
